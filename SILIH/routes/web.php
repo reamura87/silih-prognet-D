d5d901eb0f417a2f02e39940ddb738e20c7b037d
reamura87 <bramantara.24005@student.unud.ac.id>
2026-01-05 16:51:37 +0800

    dika pace

--- SILIH/routes/web.php
+++ SILIH/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PeminjamanController;
 
+
+
 Route::get('/login', [AuthController::class, 'showLogin']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
