4145d934be61100c196912ee642f3b8ca334ed98
Aryawedha <gedearyawedha@unud.ac.id>
2026-01-06 17:42:40 +0800

    last prom aw

--- resources/views/barang/create.blade.php
+++ resources/views/barang/create.blade.php
@@ -3,22 +3,35 @@
 @section('content')
 <link rel="stylesheet" href="{{ asset('css/barang.css') }}">
 
-<div class="barang-container">
-    <h2>Tambah Barang</h2>
+<div class="barang-form-wrapper">
+    <div class="barang-form-card">
+        <h2>Tambah Barang</h2>
 
-    <form action="{{ route('barang.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
+        <form action="{{ route('barang.store') }}" 
+              method="POST" 
+              enctype="multipart/form-data">
+            @csrf
 
-        <label>Nama Barang</label>
-        <input type="text" name="nama" required>
+            <div class="form-group">
+                <label>Nama Barang</label>
+                <input type="text" name="nama" required>
+            </div>
 
-        <label>Stok</label>
-        <input type="number" name="stok" min="0" required>
+            <div class="form-group">
+                <label>Stok</label>
+                <input type="number" name="stok" min="0" required>
+            </div>
 
-        <label>Gambar Barang</label>
-        <input type="file" name="gambar" accept="image/*">
+            <div class="form-group">
+                <label>Gambar Barang</label>
+                <input type="file" name="gambar" accept="image/*">
+                <small>* JPG / PNG, max 2MB</small>
+            </div>
 
-        <button type="submit">Simpan</button>
-    </form>
+            <button type="submit" class="btn-submit">
+                Simpan Barang
+            </button>
+        </form>
+    </div>
 </div>
 @endsection
